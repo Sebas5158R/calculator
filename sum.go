c59fc8055fcf436acb53173d5f5a7720d20f86b9
Sebas5158R <sebastianriveraa4@gmail.com>
2025-02-16 13:59:43 -0500

    README agregado

---
+++ sum.go
@@ -0,0 +1,19 @@
+package calculator
+
+// variable privada (inicia con minuscula), solo se puede usar dentro del paquete
+var logMessage = "[LOG]"
+
+// variable publica (inicia con mayuscula)
+var Version = "1.0"
+
+// Funcion privada (inicia con minuscula), solo se puede usar dentro del paquete
+// internalSum recibe un numero entero y devuelve el numero - 1
+func internalSum(number int) int {
+	return number - 1
+}
+
+// Funcion publica (inicia con mayuscula)
+// Sum recibe dos numeros enteros y devuelve la suma de ellos
+func Sum(number1, number2 int) int {
+	return number1 + number2
+}
